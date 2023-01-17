47af2a88c2b3a03eb8d770a811e8a82ff5ed26eb
Christopher Ashton <christopher.ashton@madetech.com>
2023-01-17 13:35:36 +0000

    completed log levels, should have imported previous methods rather than reuse the code

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -4,16 +4,27 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        int indexOfSquareBracket = logLine.IndexOf(' ');
+        string message = logLine.Substring(indexOfSquareBracket, logLine.Length - indexOfSquareBracket).Trim();
+        return message;
     }
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        int indexOfOpenSquareBracket = logLine.IndexOf('[');
+        int indexOfClosedSquareBracket = logLine.IndexOf(']');
+        string message = logLine.Substring(indexOfOpenSquareBracket + 1, indexOfClosedSquareBracket - 1).ToLower();
+        return message;
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        int indexOfOpenSquareBracket = logLine.IndexOf('[');
+        int indexOfClosedSquareBracket = logLine.IndexOf(']');
+        int indexOfSquareBracket = logLine.IndexOf(' ');
+        string messageBody = logLine.Substring(indexOfSquareBracket, logLine.Length - indexOfSquareBracket).Trim();
+        string messageError = logLine.Substring(indexOfOpenSquareBracket + 1, indexOfClosedSquareBracket - 1).ToLower();
+        string fullMessage = messageBody + $" ({messageError})";
+        return fullMessage;
     }
 }
