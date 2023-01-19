67b2d234e28aca539cd5581f993d527214581d2a
Christopher Ashton <christopher.ashton@madetech.com>
2023-01-19 17:22:09 +0000

    First pass, it's adding up way too many letters

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Diagnostics;
 
 public static class ScrabbleScore
 {
+    private static int total = 0;
+    private static char[] one = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'};
+    private static char[] two = { 'D', 'G' };
+    private static char[] three = { 'B', 'C', 'M', 'P' };
+    private static char[] four = { 'F', 'H', 'V', 'W', 'Y' };
+    private static char five = 'K';
+    private static char[] eight = { 'J', 'K' };
+    private static char[] ten = { 'Q', 'Z' };
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var c in input.ToUpper())
+        {
+            foreach (var ch in one)
+            {
+                if (c == ch)
+                {
+                    Console.WriteLine(input);
+                    Console.WriteLine(ch);
+                    total += 1;
+                }
+            }
+
+            foreach (var cb in two)
+            {
+                if (c == cb)
+                {
+                    total += 2;
+                }
+            }
+            
+            foreach (var cc in three)
+            {
+                if (c == cc)
+                {
+                    total += 3;
+                }
+            }
+            
+            foreach (var cf in four)
+            {
+                if (c == cf)
+                {
+                    total += 4;
+                }
+            }
+            
+            if (c == five)
+            {
+                total += 5;
+            }
+            
+            foreach (var cj in eight)
+            {
+                if (c == cj)
+                {
+                    total += 8;
+                }
+            }
+            
+            foreach (var cz in ten)
+            {
+                if (c == cz)
+                {
+                    total += 10;
+                }
+            }
+        }
+        Console.WriteLine(total);
+        return total;
     }
 }
