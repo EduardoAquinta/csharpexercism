3eeff6a4711081054833ddcd79b807d96627c02c
Christopher Ashton <christopher.ashton@madetech.com>
2023-01-19 11:20:16 +0000

    Completed Leap

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -4,6 +4,17 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool truth = false;
+
+        if (year % 100 == 0 && year % 400 != 0)
+        {
+            return truth;
+        }
+        if (year % 4 == 0)
+        {
+            truth = true;
+        }
+
+        return truth;
     }
 }
