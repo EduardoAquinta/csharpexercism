508104b752fe5477d137c9e439f34f70c92f63c6
Christopher Ashton <christopher.ashton@madetech.com>
2023-01-19 16:47:07 +0000

    Completed squeakyClean but with a lot of help

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Text;
 
 public static class Identifier
 {
+    
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        var cleaned  = new StringBuilder(identifier.Length);
+
+        foreach (var c in identifier)
+        {
+            if (char.IsControl(c))
+            {
+                cleaned.Append("CTRL");
+            }
+            else
+            {
+                cleaned.Append(c);
+            }
+        }
+        
+        var camelCase = new StringBuilder(cleaned.Length);
+        var adjacent = false;
+        
+        foreach (var c in cleaned.ToString())
+        {
+            if (c == '-')
+            {
+                adjacent = true;
+            }
+            else if (adjacent)
+            {
+                camelCase.Append(char.ToUpper(c));
+                adjacent = false;
+            }
+            else
+            {
+                camelCase.Append(c);
+            }
+        }
+        var lettersOnly = new StringBuilder(camelCase.Length);
+        foreach (var c in camelCase.ToString())
+        {
+            if (char.IsLetter(c))
+            {
+                lettersOnly.Append(c);
+            }
+            else if (c == ' ')
+            {
+                lettersOnly.Append('_');
+            }
+        }
+
+        var noGreekLetters = new StringBuilder(lettersOnly.Length);
+        foreach (var c in lettersOnly.ToString())
+        {
+            if (c < 'α' || c > 'ω')
+            {
+                noGreekLetters.Append(c);
+            }
+        }
+        return noGreekLetters.ToString();
     }
+    
+    
 }
