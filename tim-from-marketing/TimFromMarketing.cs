1469ac754cda8fb26efa5cef7550f41413c97011
eddie ashton <eddie.ashton@madetech.com>
2023-03-07 16:11:36 +0000

    Completed time from marketing

--- tim-from-marketing/TimFromMarketing.cs
+++ tim-from-marketing/TimFromMarketing.cs
@@ -4,6 +4,24 @@ static class Badge
 {
     public static string Print(int? id, string name, string? department)
     {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
+        var badge = "";
+        if (id == null && department == null)
+        {
+            badge = $"{name} - OWNER";
+        }
+        else if (department == null)
+        {
+            badge = $"[{id}] - {name} - OWNER";
+        }
+        else if (id == null)
+        {
+            badge = $"{name} - {department.ToUpper()}";
+        }
+        else
+        {
+            badge = $"[{id}] - {name} - {department.ToUpper()}";
+        }
+        Console.WriteLine(badge);
+        return badge;
     }
 }
